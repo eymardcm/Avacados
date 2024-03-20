8e9d69ba793274d336795ce483c1fbe3096c3206
Chad Eymard <eymardcm@gmail.com>
2024-03-19 21:12:23 -0400

    Initial Commit

---
+++ Avacados/AvacadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvacadosApp.swift
+//  Avacados
+//
+//  Created by Chad Eymard on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AvacadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
